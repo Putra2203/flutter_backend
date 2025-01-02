e0b30846f0223695cb12131b55663ced779f91f8
Putra2203 <erdin22xii@gmail.com>
2025-01-02 09:26:27 +0700

    fix login routes

--- Routes/auth.js
+++ Routes/auth.js
@@ -50,35 +50,21 @@ router.post("/register", async (req, res) => {
 // Login - Login dengan Email dan Password
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({ message: "Email and password are required" });
-  }
-
   const query = "SELECT * FROM users WHERE email = ?";
   db.query(query, [email], async (err, results) => {
-    if (err) {
-      return res.status(500).json({ message: "Database error", error: err });
-    }
-    if (results.length === 0) {
+    if (err || results.length === 0) {
       return res.status(404).json({ message: "User not found" });
     }
-
     const user = results[0];
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
-
-    // Generate token
+    
     const token = jwt.sign({ id: user.id, email: user.email }, SECRET_KEY, {
       expiresIn: "1h",
     });
-    res.status(200).json({
-      message: "Login successful",
-      token,
-      user: { id: user.id, username: user.username, email: user.email },
-    });
+    res.status(200).json({ message: "Login successful", token });
   });
 });
 
