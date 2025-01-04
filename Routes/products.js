aed7129b9e30d308224b0eb5165d186183cd1980
Putra2203 <erdin22xii@gmail.com>
2025-01-04 23:54:55 +0700

    fixing uploads dir

--- Routes/products.js
+++ Routes/products.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Konfigurasi Multer untuk upload file
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Folder tempat menyimpan file
+    cb(null, path.join(__dirname, "../uploads"));
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
